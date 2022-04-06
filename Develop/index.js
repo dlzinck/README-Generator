7b26390b6e84bc6055fe90bbc6e1428fce025397
Drew Zinck <dlzincktheii@gmail.com>
2022-04-06 15:46:30 -0700

    updated questions, linked generateMarkdown, added writeToFile and init functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,52 +1,80 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type: 'input',
-        message: 'What is your name?',
-        name: 'username',
+        type: "input",
+        message: "What is your name?",
+        name: "username"
     },
     {
-        type: 'input',
-        message: 'What is the name of your project?',
-        name: 'projectname',
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
     },
     {
-        type: 'input',
-        message: 'Briefly describe the project this README is for.',
-        name: 'description',
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title"
     },
     {
-        type: 'input',
-        message: 'Provide installation steps.',
-        name: 'install',
+        type: "input",
+        message: "Please write a short description of your project:",
+        name: "description"
     },
     {
-        type: 'input',
-        message: 'Provide examples for use.',
-        name: 'usage',
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "installation"
     },
     {
-        type: 'input',
-        message: 'List your collaborators, any third-party assests, or any other features.',
-        name: 'credits',
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "usage"
     },
     {
-        type: 'checkbox',
-        message: 'Select Licenses',
-        name: 'license',
-        choices: ['Apache License 2.0', 'GNU GPLv3', 'MIT', 'ISC License', 'GNU GPL']
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contribution"
     },
+    {
+        type: "input",
+        message: "What command should be run for testing?",
+        name: "tests"
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: [
+            "MIT",
+            "APACHE",
+            "GPL",
+            "BSD",
+            "None"
+        ]
+    }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
-
+function writeToFile(fileName, data) {
+    let content = generateMarkdown(data);
+    fs.writeFile(fileName, content, function (error) {
+        if (error) {
+            return console.log(error)
+        }
+        console.log('success')
+    });
+}
 // TODO: Create a function to initialize app
-function init() { }
-
+function init() {
+    inquirer.prompt(questions).then(function (data) {
+        var fileName = 'README.md';
+        writeToFile(fileName, data)
+    });
+}
 // Function call to initialize app
 init();
